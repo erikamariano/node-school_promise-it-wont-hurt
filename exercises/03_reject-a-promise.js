031f0d5d80589265ddd6068ea4fd163ba8b4ceeb
erika <erikammariano@gmail.com>
2021-04-22 17:57:32 -0300

    3

--- exercises/03_reject-a-promise.js
+++ exercises/03_reject-a-promise.js
@@ -6,7 +6,7 @@ let promise = new Promise((fulfilled, reject) => {
 
 promise.then(undefined, onReject);
 //First argument is undefined because the fulfill method was never called.
-//There in no need to put parenthesis on "onReject" because it is already considered a function to call.
+//There in no need to put parentheses on "onReject" because it is already considered a function to call.
 
 function onReject(error){
     console.log(error.message);
